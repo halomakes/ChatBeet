21de39f5ecca145ed91da9c1c50a58b233ea9dcb
halomademeapc <opr9029@gmail.com>
2019-07-12 18:58:16 -0400

    spin up smtp server

---
+++ ChatBeet.Smtp/SmtpListenerService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using SmtpServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatBeet.Smtp
+{
+    public class SmtpListenerService : IHostedService
+    {
+        private SmtpServer.SmtpServer server;
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var options = new SmtpServerOptionsBuilder()
+                .ServerName("localhost")
+                .Port(25, 587)
+                .MessageStore(new RadishMessageStore())
+                .Build();
+
+            server = new SmtpServer.SmtpServer(options);
+            await server.StartAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- ChatBeet/Startup.cs
+++ ChatBeet/Startup.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ChatBeet.Smtp;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,6 +27,7 @@ namespace ChatBeet
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddSmtpListener();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
