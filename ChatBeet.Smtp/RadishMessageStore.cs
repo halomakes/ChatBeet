ef4478104a4508cf77c047e452fbd5306eceb27f
halomademeapc <opr9029@gmail.com>
2019-07-12 18:58:16 -0400

    spin up smtp server

---
+++ ChatBeet.Smtp/RadishMessageStore.cs
@@ -0,0 +1,30 @@
+﻿using SmtpServer;
+using SmtpServer.Mail;
+using SmtpServer.Protocol;
+using SmtpServer.Storage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatBeet.Smtp
+{
+    internal class RadishMessageStore : MessageStore
+    {
+        public override async Task<SmtpResponse> SaveAsync(ISessionContext context, IMessageTransaction transaction, CancellationToken cancellationToken)
+        {
+            var textMessage = (ITextMessage)transaction.Message;
+            var text = string.Empty;
+
+            using (var reader = new StreamReader(textMessage.Content, Encoding.UTF8))
+            {
+                text = await reader.ReadToEndAsync();
+            }
+            Console.WriteLine(text);
+
+            return SmtpResponse.Ok;
+        }
+    }
+}
---
+++ ChatBeet.Smtp/StartupExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ChatBeet.Smtp
+{
+    public static class StartupExtensions
+    {
+        public static IServiceCollection AddSmtpListener(this IServiceCollection services)
+        {
+            services.AddHostedService<SmtpListenerService>();
+            return services;
+        }
+    }
+}
