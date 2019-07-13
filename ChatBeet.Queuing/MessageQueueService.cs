9cefef97079611f4f553f8a320a9662206b00ab0
halomademeapc <opr9029@gmail.com>
2019-07-13 00:37:55 -0400

    store history

--- ChatBeet.Queuing/MessageQueueService.cs
+++ ChatBeet.Queuing/MessageQueueService.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChatBeet.Queuing
 {
     public class MessageQueueService : IMessageQueueService
     {
-        private QueueConfigurationAccessor configurationAccessor;
+        private readonly QueueConfigurationAccessor configurationAccessor;
+        private const int MAX_HISTORY = 300;
 
         public MessageQueueService(QueueConfigurationAccessor configurationAccessor)
         {
@@ -13,10 +15,12 @@ namespace ChatBeet.Queuing
         }
 
         private List<IQueuedMessageSource> queuedMessages = new List<IQueuedMessageSource>();
+        private List<IQueuedMessageSource> messageHistory = new List<IQueuedMessageSource>();
 
         public event EventHandler MessageAdded;
 
         public List<IQueuedMessageSource> ViewAll() => queuedMessages;
+        public List<IQueuedMessageSource> GetHistory() => messageHistory;
 
         public List<IQueuedMessageSource> PopAll()
         {
@@ -28,12 +32,21 @@ namespace ChatBeet.Queuing
         public void Push(IQueuedMessageSource message)
         {
             queuedMessages.Add(message);
+            messageHistory.Add(message);
+            TrimHistory();
             OnMessageAdded(EventArgs.Empty);
         }
 
         private void OnMessageAdded(EventArgs e)
         {
             MessageAdded?.Invoke(this, e);
+        }
+
+        private void TrimHistory()
+        {
+            var overflow = MAX_HISTORY - messageHistory.Count;
+            if (overflow > 0)
+                messageHistory = messageHistory.Skip(overflow).Take(MAX_HISTORY).ToList();
         }
     }
 }
