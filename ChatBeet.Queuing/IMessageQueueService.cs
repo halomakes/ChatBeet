d3c4a70f7db21d201d7b6ad45401e4b02de604f4
halomademeapc <opr9029@gmail.com>
2019-07-15 21:40:26 -0400

    set up api, forms

--- ChatBeet.Queuing/IMessageQueueService.cs
+++ ChatBeet.Queuing/IMessageQueueService.cs
@@ -11,5 +11,6 @@ namespace ChatBeet.Queuing
         event EventHandler MessageAdded;
         List<IQueuedMessageSource> GetHistory();
         List<OutputMessage> GetOutputHistory();
+        void PushRaw(OutputMessage message);
     }
 }
