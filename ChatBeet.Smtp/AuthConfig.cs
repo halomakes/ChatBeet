72b54a78a54430849bb6818ed0981ab572b39244
halomademeapc <alex@halomademeapc.com>
2019-07-17 09:01:23 -0400

    pull smtp config from appsettings

---
+++ ChatBeet.Smtp/AuthConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ChatBeet.Smtp
+{
+    public class AuthConfig
+    {
+        public bool AllowAny { get; set; }
+        public IEnumerable<CredentialSet> AuthorizedUsers { get; set; }
+    }
+}
