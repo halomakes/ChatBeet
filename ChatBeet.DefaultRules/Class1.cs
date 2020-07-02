8c1769567c27776e9a8baff57fed4a195ef08296
halomademeapc <alex@halomademeapc.com>
2020-07-02 11:31:23 -0400

    start configuring rules

--- ChatBeet.DefaultRules/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ChatBeet.DefaultRules
-{
-    public class Class1
-    {
-    }
-}
--- ChatBeet/ExceptionMessage.cs
+++ ChatBeet/ExceptionMessage.cs
@@ -1,29 +1,14 @@
 ﻿using System;
-using System.Collections;
 
 namespace ChatBeet
 {
-    public class Class1
-    {
-    }
-
-    public class IrcMessage : IInboundMessage
-    {
-        public string Sender { get; set; }
-
-        public string Channel { get; set; }
-
-        public string Content { get; set; }
-
-        public DateTime DateRecieved { get; set; }
-    }
-
     public class ExceptionMessage : IInboundMessage
     {
         public ExceptionMessage(Exception ex)
         {
             Sender = ex.Source;
             Content = ex.Message;
+            Exception = ex;
         }
 
         public string Sender { get; }
@@ -31,5 +16,7 @@ namespace ChatBeet
         public string Content { get; }
 
         public DateTime DateRecieved { get; } = DateTime.Now;
+
+        public Exception Exception { get; }
     }
 }
