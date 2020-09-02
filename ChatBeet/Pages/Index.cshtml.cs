6039592666ae8acf76b868ae72ade166fc1ebcdf
halomademeapc <alex@halomademeapc.com>
2020-09-02 14:12:39 -0400

    show readme on home page

--- ChatBeet/Pages/Index.cshtml.cs
+++ ChatBeet/Pages/Index.cshtml.cs
@@ -1,25 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Markdig;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ChatBeet.Pages
 {
+    [ResponseCache(VaryByHeader = "User-Agent", Duration = 30)]
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ILogger<IndexModel> logger;
+        private readonly IMemoryCache cache;
+        private readonly IWebHostEnvironment env;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public string ReadmeHtml { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IMemoryCache cache, IWebHostEnvironment env)
         {
-            _logger = logger;
+            this.logger = logger;
+            this.cache = cache;
+            this.env = env;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            var readmeMarkdown = await cache.GetOrCreateAsync("readme", async entry =>
+            {
+                entry.SlidingExpiration = TimeSpan.FromMinutes(15);
 
+                var filepath = Path.Combine(env.WebRootPath, "README.md");
+                return await System.IO.File.ReadAllTextAsync(filepath);
+            });
+
+            ReadmeHtml = Markdown.ToHtml(readmeMarkdown);
         }
     }
 }
