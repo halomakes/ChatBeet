03286de07fb7bf928a5db135726998f1320aae47
halomademeapc <alex@halomademeapc.com>
2020-09-08 16:49:11 -0400

    cleanup

--- ChatBeet/Models/LoginTokenRequest.cs
+++ ChatBeet/Models/LoginTokenRequest.cs
@@ -6,6 +6,8 @@ namespace ChatBeet.Models
     {
         [RegularExpression(@"^[a-z0-9_\-\[\]\\^{}|`]{2,16}$")]
         public string Nick { get; set; }
+
+        [Display(Name = "Authentication Token")]
         public string AuthToken { get; set; }
     }
 }
