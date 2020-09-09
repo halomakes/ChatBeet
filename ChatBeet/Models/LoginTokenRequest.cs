4e237c540e4f23692ffda40d012a65809476fc55
halomademeapc <alex@halomademeapc.com>
2020-09-09 12:47:06 -0400

    make nick required

--- ChatBeet/Models/LoginTokenRequest.cs
+++ ChatBeet/Models/LoginTokenRequest.cs
@@ -4,7 +4,7 @@ namespace ChatBeet.Models
 {
     public class LoginTokenRequest
     {
-        [RegularExpression(@"^[A-z0-9_\-\[\]\\^{}|`]{2,16}$", ErrorMessage = "Enter a valid IRC nick.")]
+        [Required, RegularExpression(@"^[A-z0-9_\-\[\]\\^{}|`]{2,16}$", ErrorMessage = "Enter a valid IRC nick.")]
         public string Nick { get; set; }
 
         [Display(Name = "Authentication Token")]
