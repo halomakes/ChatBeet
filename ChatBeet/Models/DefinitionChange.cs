bd40885d1550191eb412a77de1aa5991e22b1ff3
halomademeapc <alex@halomademeapc.com>
2020-09-09 11:54:35 -0400

    add def editing

---
+++ ChatBeet/Models/DefinitionChange.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatBeet.Models
+{
+    public class DefinitionChange
+    {
+        [Required, MaxLength(250), Display(Prompt = "astolfo")]
+        public string Key { get; set; }
+
+        [Required, MaxLength(500), Display(Name = "Value", Prompt = "kyoot")]
+        public string NewValue { get; set; }
+
+        public string OldValue { get; set; }
+
+        public string NewNick { get; set; }
+
+        public string OldNick { get; set; }
+    }
+}
---
+++ ChatBeet/Rules/DefUpdatedRule.cs
@@ -0,0 +1,27 @@
+﻿using ChatBeet.Models;
+using GravyBot;
+using GravyIrc.Messages;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+
+namespace ChatBeet.Rules
+{
+    public class DefUpdatedRule : MessageRuleBase<DefinitionChange>
+    {
+        private readonly IrcBotConfiguration config;
+
+        public DefUpdatedRule(IOptions<IrcBotConfiguration> opts)
+        {
+            config = opts.Value;
+        }
+
+        public override IEnumerable<IClientMessage> Respond(DefinitionChange incomingMessage)
+        {
+            yield return new PrivateMessage(config.NotifyChannel, $"{IrcValues.BOLD}{incomingMessage.NewNick}{IrcValues.RESET} set {IrcValues.BOLD}{incomingMessage.Key}{IrcValues.RESET} = {incomingMessage.NewValue}");
+            if (!string.IsNullOrEmpty(incomingMessage.OldValue))
+            {
+                yield return new PrivateMessage(config.NotifyChannel, $"Previous value was {IrcValues.BOLD}{incomingMessage.OldValue}{IrcValues.RESET}, set by {incomingMessage.OldNick}.");
+            }
+        }
+    }
+}
