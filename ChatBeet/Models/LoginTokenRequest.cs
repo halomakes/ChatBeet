da7d7cf34946c2c641ebdb365c33708cca029b5c
halomademeapc <alex@halomademeapc.com>
2020-09-08 21:53:28 -0400

    fix navbar

--- ChatBeet/Models/LoginTokenRequest.cs
+++ ChatBeet/Models/LoginTokenRequest.cs
@@ -4,7 +4,7 @@ namespace ChatBeet.Models
 {
     public class LoginTokenRequest
     {
-        [RegularExpression(@"^[a-z0-9_\-\[\]\\^{}|`]{2,16}$")]
+        [RegularExpression(@"^[A-z0-9_\-\[\]\\^{}|`]{2,16}$", ErrorMessage = "Enter a valid IRC nick.")]
         public string Nick { get; set; }
 
         [Display(Name = "Authentication Token")]
