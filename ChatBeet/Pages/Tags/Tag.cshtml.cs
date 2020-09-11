927e76cd1a543d64875ff83f28bcfb03ebbf52ef
halomademeapc <alex@halomademeapc.com>
2020-09-10 21:55:30 -0400

    add tag-specific stats

--- ChatBeet/Pages/Tags/Tag.cshtml.cs
+++ ChatBeet/Pages/Tags/Tag.cshtml.cs
@@ -1,19 +1,45 @@
-﻿using System;
+﻿using ChatBeet.Data;
+using ChatBeet.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ChatBeet.Pages.Tags
 {
     public class TagModel : PageModel
     {
+        private readonly BooruContext booru;
+        private readonly IMemoryCache cache;
+
         public string TagName { get; private set; }
+        public IEnumerable<TagStat> Stats { get; private set; }
+
+        public TagModel(BooruContext booru, IMemoryCache cache)
+        {
+            this.booru = booru;
+            this.cache = cache;
+        }
 
         public async Task OnGet(string tagName)
         {
-            TagName = tagName.ToLower();
+            TagName = tagName.Trim().ToLower();
+            var matchingTags = await cache.GetOrCreateAsync($"tags:tag:{tagName}", async entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1);
+
+                return await booru.TagHistories
+                    .AsQueryable()
+                    .Where(th => th.Tag.ToLower() == tagName)
+                    .GroupBy(th => th.Nick)
+                    .OrderByDescending(g => g.Count())
+                    .Select(g => new TagStat { Tag = g.Key, Total = g.Count() })
+                    .ToListAsync();
+            });
+            Stats = matchingTags;
         }
     }
 }
