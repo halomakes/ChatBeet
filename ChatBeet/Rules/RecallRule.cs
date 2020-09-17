6aa6d4d2e975770cb00f8669f25cdc312a069d35
halomademeapc <alex@halomademeapc.com>
2020-09-17 09:57:59 -0400

    add negative lookahead

--- ChatBeet/Rules/RecallRule.cs
+++ ChatBeet/Rules/RecallRule.cs
@@ -21,7 +21,7 @@ namespace ChatBeet.Rules
             config = options.Value;
             this.ctx = ctx;
             commandingRgx = new Regex($"^(?:{Regex.Escape(config.Nick)},? |{Regex.Escape(config.CommandPrefix)})(?:recall|tell me about|show me) (.+)", RegexOptions.IgnoreCase);
-            interrogativeRgx = new Regex($"^(?:{Regex.Escape(config.Nick)},? |{Regex.Escape(config.CommandPrefix)})(?:what['ʼ]?(?:s|re)|(?:what|who) (?:is|are)|what do you know about) (.+)", RegexOptions.IgnoreCase);
+            interrogativeRgx = new Regex($"^(?:{Regex.Escape(config.Nick)},? |{Regex.Escape(config.CommandPrefix)})(?:(?!what['ʼ]?s new from)what['ʼ]?(?:s|re)|(?:what|who) (?:is|are)|what do you know about) (.+)", RegexOptions.IgnoreCase);
         }
 
         public override bool Matches(PrivateMessage incomingMessage) => commandingRgx.IsMatch(incomingMessage.Message) || interrogativeRgx.IsMatch(incomingMessage.Message);
