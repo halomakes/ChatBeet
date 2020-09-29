bc4bfb4c01740b12b0a2b8a837780fc97f51330b
halomademeapc <alex@halomademeapc.com>
2020-09-28 21:48:27 -0400

    include complication details

--- ChatBeet/Rules/CurrentWeatherRule.cs
+++ ChatBeet/Rules/CurrentWeatherRule.cs
@@ -1,4 +1,5 @@
-﻿using ChatBeet.Data.Entities;
+﻿using ChatBeet.Configuration;
+using ChatBeet.Data.Entities;
 using ChatBeet.Services;
 using ChatBeet.Utilities;
 using GravyBot;
@@ -10,6 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using UnitsNet;
 using UnitsNet.Units;
 using OpenWeatherMapClient = OpenWeatherMap.Standard.Current;
 
@@ -56,11 +58,47 @@ namespace ChatBeet.Rules
                 var currentConditions = await wmClient.GetWeatherDataByZipAsync(zip, "US");
                 if (currentConditions != default)
                 {
+                    var complicationDetails = new List<string>();
+
                     var windUnit = await prefsService.Get(incomingMessage.From, UserPreference.WeatherWindUnit, SpeedUnit.MilePerHour, SpeedUnit.Undefined);
                     var tempUnit = await prefsService.Get(incomingMessage.From, UserPreference.WeatherTempUnit, TemperatureUnit.DegreeFahrenheit, TemperatureUnit.Undefined);
                     var precipUnit = await prefsService.Get(incomingMessage.From, UserPreference.WeatherPrecipUnit, LengthUnit.Inch, LengthUnit.Undefined);
 
-                    yield return new PrivateMessage(incomingMessage.GetResponseTarget(), $"Current conditions for {IrcValues.BOLD}{currentConditions.Name}{IrcValues.RESET}");
+                    // temperature
+                    string getTemp(float c) => Temperature.FromDegreesCelsius(c).ToUnit(tempUnit).ToString();
+                    complicationDetails.Add($"🌡{IrcValues.BOLD}{getTemp(currentConditions.WeatherDayInfo.Temperature)}{IrcValues.RESET}{IrcValues.RED} ⬆{getTemp(currentConditions.WeatherDayInfo.MaximumTemperature)}{IrcValues.BLUE} ⬇{getTemp(currentConditions.WeatherDayInfo.MinimumTemperature)}{IrcValues.RESET}");
+
+                    // humidity
+                    var idealHumidity = 40;
+                    var comfortRating = Math.Abs(100 - Math.Abs(currentConditions.WeatherDayInfo.Humidity - idealHumidity));
+                    complicationDetails.Add($"Humidity: {$"{currentConditions.WeatherDayInfo.Humidity}%".Colorize(comfortRating)}");
+
+                    string getPrecipitation(float c) => string.Format("{0:s2}", Length.FromMillimeters(c).ToUnit(precipUnit));
+
+                    // conditional rain
+                    if (currentConditions.Rain != default)
+                    {
+                        complicationDetails.Add($"{getPrecipitation(currentConditions.Rain.LastHour)} rain in past hour");
+                    }
+
+                    // conditional snow
+                    if (currentConditions.Snow != default)
+                    {
+                        complicationDetails.Add($"{getPrecipitation(currentConditions.Snow.LastHour)} snow in past hour");
+                    }
+
+                    // conditional wind
+                    if (currentConditions.Wind != default)
+                    {
+                        string getWindspeed(float c) => string.Format("{0:s1}", Speed.FromMetersPerSecond(c).ToUnit(windUnit));
+                        string windDirection = Angle.FromDegrees(currentConditions.Wind.Degree).ToCardinalDirection();
+                        complicationDetails.Add($"Wind: {getWindspeed(currentConditions.Wind.Speed)} {windDirection}");
+                    }
+
+                    // conditions
+                    complicationDetails.Add(string.Join(", ", currentConditions.Weathers.Select(w => $"{w.GetEmoji(currentConditions.DayInfo)} {w.Description}")));
+
+                    yield return new PrivateMessage(incomingMessage.GetResponseTarget(), $"Right now in {IrcValues.BOLD}{currentConditions.Name}{IrcValues.RESET}: {string.Join(" | ", complicationDetails)}");
                 }
                 else
                 {
@@ -72,5 +110,7 @@ namespace ChatBeet.Rules
                 yield return new PrivateMessage(incomingMessage.GetResponseTarget(), "Please specify a ZIP code or set a default one in your user preferences.");
             }
         }
+
+
     }
 }
