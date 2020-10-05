09e86d7b53c445491a15fab2dfba354ec90f81f5
Alex Griffith <halomademeapc@users.noreply.github.com>
2020-10-05 15:59:57 -0400

    HTML decode tweet message

--- ChatBeet/Utilities/TwitterExtensions.cs
+++ ChatBeet/Utilities/TwitterExtensions.cs
@@ -1,11 +1,12 @@
 ﻿using GravyBot;
 using Humanizer;
 using LinqToTwitter;
+using System.Web;
 
 namespace ChatBeet.Utilities
 {
     public static class TwitterExtensions
     {
-        public static string ToIrcMessage(this Status tweet) => $"{IrcValues.BOLD}{tweet.User?.Name}{IrcValues.RESET} {tweet.CreatedAt.Humanize()} - {(tweet.FullText ?? tweet.Text)?.RemoveLineBreaks(" | ")}";
+        public static string ToIrcMessage(this Status tweet) => $"{IrcValues.BOLD}{tweet.User?.Name}{IrcValues.RESET} {tweet.CreatedAt.Humanize()} - {(HttpUtility.HtmlDecode(tweet.FullText ?? tweet.Text).RemoveLineBreaks(" | ")}";
     }
 }
