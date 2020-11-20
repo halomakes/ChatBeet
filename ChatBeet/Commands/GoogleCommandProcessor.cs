fcacd49bcf25247355f74be4377c14f74f5b071a
halomademeapc <alex@halomademeapc.com>
2020-11-19 19:31:16 -0500

    start parsing urls

--- ChatBeet/Commands/GoogleCommandProcessor.cs
+++ ChatBeet/Commands/GoogleCommandProcessor.cs
@@ -1,8 +1,11 @@
 ﻿using ChatBeet.Utilities;
+using GravyBot;
 using GravyBot.Commands;
 using GravyIrc.Messages;
 using Microsoft.Extensions.Caching.Memory;
+using OpenGraphNet;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -25,12 +28,34 @@ namespace ChatBeet.Commands
 
         [Command("google {query}", Description = "Look something up on Google using I'm Feeling Lucky")]
         [Command("feelinglucky {query}", Description = "Look something up on Google using I'm Feeling Lucky")]
-        public async Task<IClientMessage> Search(string query)
+        public async IAsyncEnumerable<IClientMessage> Search(string query)
         {
             var uriBuilder = new UriBuilder($"https://www.google.com/search?btnI=1&q={WebUtility.UrlEncode(query)}");
             var resultLink = await TryGetTargetLink(uriBuilder.Uri);
+            if (!resultLink.Host.Contains("google.com", StringComparison.OrdinalIgnoreCase))
+            {
+                // not a google link, try to generate preview
+                var metaTask = OpenGraph.ParseUrlAsync(resultLink);
+                var timeoutTask = Task.Delay(TimeSpan.FromSeconds(2));
+                await Task.WhenAny(metaTask, timeoutTask);
 
-            return new PrivateMessage(IncomingMessage.GetResponseTarget(), $"{IncomingMessage.From}: {resultLink}");
+                if (metaTask.IsCompleted)
+                {
+                    var meta = metaTask.Result;
+                    yield return new PrivateMessage(IncomingMessage.GetResponseTarget(), $"{IncomingMessage.From}: {resultLink} {meta.ToIrcSummary()}");
+                }
+                else
+                {
+                    // pinging page is taking too long, go ahead and give url then follow up with metadata later
+                    yield return new PrivateMessage(IncomingMessage.GetResponseTarget(), $"{IncomingMessage.From}: {resultLink}");
+                    var meta = await metaTask;
+                    yield return new PrivateMessage(IncomingMessage.GetResponseTarget(), meta.ToIrcSummary(maxDescriptionLength: 80));
+                }
+            }
+            else
+            {
+                yield return new PrivateMessage(IncomingMessage.GetResponseTarget(), $"{IncomingMessage.From}: {resultLink}");
+            }
         }
 
         private async Task<Uri> TryGetTargetLink(Uri feelingLuckyUri)
---
+++ ChatBeet/Utilities/OpenGraphExtensions.cs
@@ -0,0 +1,35 @@
+﻿using GravyBot;
+using OpenGraphNet;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChatBeet.Utilities
+{
+    public static class OpenGraphExtensions
+    {
+        public static string ToIrcSummary(this OpenGraph meta, int maxDescriptionLength = 50)
+        {
+            IEnumerable<string> GetSegments()
+            {
+                if (meta.Metadata.ContainsKey("Title"))
+                {
+                    var title = meta.Metadata["Title"].Select(m => m.Value?.Trim()).FirstOrDefault(v => !string.IsNullOrEmpty(v));
+                    if (title != default)
+                        yield return $"{IrcValues.BOLD}{title}{IrcValues.RESET}";
+                }
+                if (meta.Metadata.ContainsKey("Description"))
+                {
+                    var description = meta.Metadata["Description"].Select(m => m.Value?.Trim()).FirstOrDefault(v => !string.IsNullOrEmpty(v));
+                    if (description != default)
+                    {
+                        if (description.Length > maxDescriptionLength)
+                            yield return $"{description.Substring(0, maxDescriptionLength)}…";
+                        else
+                            yield return description;
+                    }
+                }
+            }
+            return string.Join(" | ", GetSegments());
+        }
+    }
+}
