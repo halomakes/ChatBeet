fa09a0f2ab8991514b52c5723f649c7e80f3e056
halomademeapc <alex@halomademeapc.com>
2021-05-09 14:54:15 -0400

    scaffold time ranges ui pages

---
+++ ChatBeet/Pages/FixedTimeRanges/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using ChatBeet.Data;
+using ChatBeet.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBeet.Pages.FixedTimeRanges
+{
+    [Authorize]
+    public class DeleteModel : PageModel
+    {
+        private readonly ProgressContext _context;
+
+        public DeleteModel(ProgressContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public FixedTimeRange FixedTimeRange { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            FixedTimeRange = await _context.FixedTimeRanges.AsQueryable().FirstOrDefaultAsync(m => m.Key == id);
+
+            if (FixedTimeRange == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            FixedTimeRange = await _context.FixedTimeRanges.FindAsync(id);
+
+            if (FixedTimeRange != null)
+            {
+                _context.FixedTimeRanges.Remove(FixedTimeRange);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ChatBeet/Pages/FixedTimeRanges/Details.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using ChatBeet.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBeet.Pages.FixedTimeRanges
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Data.ProgressContext _context;
+
+        public DetailsModel(Data.ProgressContext context)
+        {
+            _context = context;
+        }
+
+        public FixedTimeRange FixedTimeRange { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            FixedTimeRange = await _context.FixedTimeRanges.AsQueryable().FirstOrDefaultAsync(m => m.Key == id);
+
+            if (FixedTimeRange == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
