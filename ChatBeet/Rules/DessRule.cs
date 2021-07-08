d22e4f3fa4561f3f729ad2313ce426ff0432c3f6
halomademeapc <alex@halomademeapc.com>
2021-07-08 15:47:32 -0400

    add dess command

---
+++ ChatBeet/Rules/DessRule.cs
@@ -0,0 +1,24 @@
+﻿using ChatBeet.Commands;
+using GravyBot;
+using GravyBot.Commands;
+using GravyIrc.Messages;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ChatBeet.Rules
+{
+    public class DessRule : CommandAliasRule<BooruCommandProcessor>
+    {
+        public DessRule(IOptions<IrcBotConfiguration> options, IServiceProvider serviceProvider) : base(options, serviceProvider)
+        {
+            Pattern = new Regex(@"!dess", RegexOptions.IgnoreCase);
+        }
+
+        protected override async IAsyncEnumerable<IClientMessage> OnMatch(Match match, BooruCommandProcessor commandProcessor)
+        {
+            yield return await commandProcessor.GetRandomPost("akatsuki_kirika");
+        }
+    }
+}
