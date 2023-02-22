e778a2642011e5646261ed8fac145f6b6e940335
Alex Griffith <alex@alexgriffith.me>
2023-02-22 10:40:10 -0500

    attempt to set up modal

--- ChatBeet/Commands/MessageTransformCommandProcessor.cs
+++ ChatBeet/Commands/MessageTransformCommandProcessor.cs
@@ -20,6 +20,39 @@ public partial class MessageTransformCommandModule : ApplicationCommandModule
         _client = client;
     }
 
+    [ContextMenu(ApplicationCommandType.MessageContextMenu, "Transform")]
+    public async Task TransformMessage(ContextMenuContext ctx)
+    {
+        await ctx.CreateResponseAsync(InteractionResponseType.Modal, new DiscordInteractionResponseBuilder()
+            .WithTitle("Verify Your Account")
+            .WithCustomId($"message-transform-{ctx.TargetMessage.Id}")
+            .AddComponents(new DiscordSelectComponent("replaceType", "Basic", options: new[]
+            {
+                new DiscordSelectComponentOption("Basic", "basic", "Use basic text replacement", true),
+                new DiscordSelectComponentOption("Regular Expressions", "regex", "Use regular expressions in replacement")
+            }))
+            .AddComponents(new TextInputComponent("Text to Replace", "replacePattern", required: false))
+            .AddComponents(new TextInputComponent("Replacement Text", "replaceValue", placeholder: "poop", value: "poop", required: false))
+            .AddComponents(new DiscordSelectComponent("scramble", "Do Not Scramble", options: new[]
+            {
+                new DiscordSelectComponentOption("Do Not Scramble", "false", "Leave the order of words alone", true),
+                new DiscordSelectComponentOption("Scramble Words", "true", "around the Mix order words of")
+            }))
+            .AddComponents(new DiscordSelectComponent("mock", "Do Not Mock", options: new[]
+            {
+                new DiscordSelectComponentOption("Do Not Mock", "false", "Leave the text case alone", true),
+                new DiscordSelectComponentOption("Mock", "true", "rAndOmiZE tHe cAsE Of tHE mESsaGe tExT")
+            }))
+            .AddComponents(new DiscordSelectComponent("kern", "Do Not Kern", options: new[]
+            {
+                new DiscordSelectComponentOption("Do Not Kern", "false", "Leave text spacing alone", true),
+                new DiscordSelectComponentOption("Kern Text", "true", "S p a c e  l e t t e r s  a p a r t")
+            }))
+            .WithContent($@"Fill out the fields below to specify how to modify this message
+{Formatter.BlockCode(ctx.TargetMessage.Content)}")
+        );
+    }
+
     [GeneratedRegex(@"([\x00-\x7F])")]
     private static partial Regex SpacingRegex();
 
