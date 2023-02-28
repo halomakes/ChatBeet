bf892b316c6f136e3686db69c142f6379c14476e
halomademeapc <alex@halomademeapc.com>
2023-02-27 21:58:31 -0500

    add emoji react to whip command

---
+++ ChatBeet/Handlers/WhipReactHandler.cs
@@ -0,0 +1,42 @@
+using System.Threading;
+using System.Threading.Tasks;
+using ChatBeet.Commands;
+using ChatBeet.Notifications;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChatBeet.Handlers;
+
+public class WhipReactHandler : INotificationHandler<DiscordNotification<MessageReactionAddEventArgs>>
+{
+    private static ulong _userId;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public WhipReactHandler(IServiceScopeFactory serviceScopeFactory)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    public async Task Handle(DiscordNotification<MessageReactionAddEventArgs> notification, CancellationToken cancellationToken)
+    {
+        if (notification.Event.Message.Author.Id != GetUserId() || notification.Event.User.Id == GetUserId() || notification.Event.Emoji.Name != WhipCommandModule.Emoji)
+            return;
+        if (!WhipCommandModule.CanUpdate(notification.Event.Message))
+            return;
+
+        await WhipCommandModule.UpdateComparison(notification.Event.Channel.Id, notification.Event.User);
+    }
+
+    private ulong GetUserId()
+    {
+        if (_userId != default)
+            return _userId;
+
+        using var scope = _serviceScopeFactory.CreateScope();
+        var client = scope.ServiceProvider.GetRequiredService<DiscordClient>();
+        _userId = client.CurrentUser.Id;
+        return _userId;
+    }
+}
